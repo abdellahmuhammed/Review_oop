9936be3148e861331bbf17c56301a77574c1708d
abdellahmuhammed <abdellah.mohamedcs#gmail.com>
2024-07-25 13:40:08 +0300

    firts

--- Human.dart
+++ Human.dart
@@ -2,30 +2,64 @@ class Human {
   double? height;
   double? weight;
   String? SkinColor;
-  int? _numberOfArm;
+  int _numberOfArms = 2;
 
-  // how to create constractor
+  // how to create constructor
+  Human({
+    this.height,
+    this.weight,
+    this.SkinColor,
+  });
+
+/*
+
+ //another way to create constructor
   Human({
     double? height,
     double? weight,
     String? SkinColor,
-    int numberOfArm = 2,
   }) {
     this.height = height;
     this.weight = weight;
     this.SkinColor = SkinColor;
-    this._numberOfArm = _numberOfArm;
   }
 
+*/
+
+  // important notes
   // any function in class is method
+  // to do encapsulation do it
 
-  // create methods
+  // 1- make _ before your var
+  //2- make set function
+  //3- make get function
 
-  void Run() {
-    print(' Human Height is $weight');
+/* 
+void  setNumberOfArmS(int numberOfArms) {
+    if (numberOfArms >= 0 && numberOfArms <= 2) {
+      this._numberOfArms = numberOfArms;
+      print('your Number Of Arms is ${_numberOfArms}');
+    } else {
+      this._numberOfArms = _numberOfArms;
+      print(
+          'you Enter invalid Number Of Arms and your Number Of Arms is  ${_numberOfArms}');
+    }
+  }
+*/
+
+  set setNumberOfArmS(int numberOfArms) {
+    if (numberOfArms >= 0 && numberOfArms <= 2) {
+      this._numberOfArms = numberOfArms;
+      print('your Number Of Arms is ${_numberOfArms}');
+    } else {
+      this._numberOfArms = _numberOfArms;
+      print(
+          'you Entern invalid Number Of Arms and your Number Of Arms is  ${_numberOfArms}');
+    }
   }
 
-  void walk() {
-    print('Any Human can walk ');
-  }
+// طريقة تانية
+  // int getNumberOfArms() => this._numberOfArms;
+
+  int get getNumberOfArms => this._numberOfArms;
 }
--- main.dart
+++ main.dart
@@ -1,19 +1,15 @@
-import 'class Human {.dart';
+import 'Human.dart';
 
-void main(List<String> args) {
-  // create object and it name is Abdellah
-  Human Abdellah = Human(height: 150.0, weight: 225, numberOfArm: 25);
+void main() {
+  // create object from class Human and it name is Abdellah
+  Human Abdellah = Human(height: 150.0, weight: 225);
+  Abdellah.setNumberOfArmS = 2;
+  // print(Abdellah.getNumberOfArms);
 
   print(Abdellah.height);
-  Abdellah.Run();
-  Abdellah.walk();
 
   // create anther object and it name is Abdellah
   Human Abdo = Human(height: 181.0, weight: 250);
 
   print(Abdo.height);
-  Abdo.Run();
-  Abdo.walk();
 }
-
-// how to create class
